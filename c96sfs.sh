a7f427e919777b5ac0db867fad625c6936854d73
Wei Huang <Wei.Huang@noaa.gov>
2024-10-16 12:21:10 -0500

    run first 720 hours OK

--- c96sfs.sh
+++ c96sfs.sh
@@ -8,7 +8,7 @@
  HPC_ACCOUNT=epic \
         IDATE=1994050100 \
         pslot=c96sfs \
-        RUNTESTS=/work2/noaa/da/weihuang/run \
+        RUNTESTS=/work2/noaa/gsienkf/weihuang/run \
         ./workflow/create_experiment.py \
         --yaml SFS_baseline-c96/SFS.yaml
 
