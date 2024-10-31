4b78122885a336aa697f7be9a38ec23b8ac19a41
Wei Huang <Wei.Huang@noaa.gov>
2024-10-31 21:07:29 +0000

    testing re-run

--- scripts/exglobal_cleanup.sh
+++ scripts/exglobal_cleanup.sh
@@ -7,15 +7,15 @@ echo "Begin Cleanup ${DATAROOT}!"
 
 # Remove DATAoutput from the forecast model run
 # TODO: Handle this better
-DATAfcst="${DATAROOT}/${RUN}fcst.${PDY:-}${cyc}"
-if [[ -d "${DATAfcst}" ]]; then rm -rf "${DATAfcst}"; fi
+#DATAfcst="${DATAROOT}/${RUN}fcst.${PDY:-}${cyc}"
+#if [[ -d "${DATAfcst}" ]]; then rm -rf "${DATAfcst}"; fi
 #DATAefcs="${DATAROOT}/${RUN}efcs???${PDY:-}${cyc}"
-rm -rf "${DATAROOT}/${RUN}efcs"*"${PDY:-}${cyc}"
+#rm -rf "${DATAROOT}/${RUN}efcs"*"${PDY:-}${cyc}"
 ###############################################################
 
-if [[ "${CLEANUP_COM:-YES}" == NO ]] ; then
+#if [[ "${CLEANUP_COM:-YES}" == NO ]] ; then
     exit 0
-fi
+#fi
 
 ###############################################################
 # Clean up previous cycles; various depths
